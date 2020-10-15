5d9dbd81bc0f4d153ad481f0207d7a2037c8e6f2
MegaMech <MegaMech@users.noreply.github.com>
2020-10-14 20:01:13 -0700

    Update custom_menu.c

--- src/menu/custom_menu.c
+++ src/menu/custom_menu.c
@@ -42,7 +42,7 @@ static void menu_main_draw_strings(void) {
 
 static void host_menu_draw_strings(void) {
     #ifdef DISCORD_SDK
-        #define HOST_MENU_MAX_ITEMS 5
+        #define HOST_MENU_MAX_ITEMS 4
     #else
         #define HOST_MENU_MAX_ITEMS 3
     #endif
