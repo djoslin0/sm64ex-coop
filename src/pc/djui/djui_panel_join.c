f1d030268ea788db1a716cbefc5910c1d4bb2291
Isaac <62234577+Isaac0-dev@users.noreply.github.com>
2022-02-21 16:21:00 +1000

    Update djui_panel_join.c

--- src/pc/djui/djui_panel_join.c
+++ src/pc/djui/djui_panel_join.c
@@ -136,11 +136,11 @@ static void djui_panel_join_ip_text_set(struct DjuiInputbox* inputbox1) {
 }
 
 void djui_panel_join_do_join(struct DjuiBase* caller) {
-//     if (!djui_panel_join_ip_valid(sInputboxIp->buffer)) {
-//         djui_interactable_set_input_focus(&sInputboxIp->base);
-//         djui_inputbox_select_all(sInputboxIp);
-//         return;
-//     }
+    // if (!djui_panel_join_ip_valid(sInputboxIp->buffer)) {
+    //     djui_interactable_set_input_focus(&sInputboxIp->base);
+    //     djui_inputbox_select_all(sInputboxIp);
+    //     return;
+    // }
     djui_panel_join_ip_text_set_new();
     network_set_system(NS_SOCKET);
     network_init(NT_CLIENT);
