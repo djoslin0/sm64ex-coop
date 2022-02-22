7946bce7e14fb6d0bbe3b57c50607a9b7fec7116
Isaac <62234577+Isaac0-dev@users.noreply.github.com>
2022-02-22 11:58:11 +1000

    Update configfile.c

--- src/pc/configfile.c
+++ src/pc/configfile.c
@@ -184,7 +184,7 @@ static const struct ConfigOption options[] = {
     {.name = "coop_join_port",                 .type = CONFIG_TYPE_UINT  , .uintValue   = &configJoinPort},
     {.name = "coop_host_port",                 .type = CONFIG_TYPE_UINT  , .uintValue   = &configHostPort},
     {.name = "coop_host_save_slot",            .type = CONFIG_TYPE_UINT  , .uintValue   = &configHostSaveSlot},
-    {.name = "coop_forced_warp",        .type = CONFIG_TYPE_BOOL  , .boolValue   = &configforcewarp},
+    {.name = "coop_forced_warp",               .type = CONFIG_TYPE_BOOL  , .boolValue   = &configforcewarp},
     {.name = "coop_player_interaction",        .type = CONFIG_TYPE_UINT  , .uintValue   = &configPlayerInteraction},
     {.name = "coop_player_knockback_strength", .type = CONFIG_TYPE_UINT  , .uintValue   = &configPlayerKnockbackStrength},
     {.name = "coop_stay_in_level_after_star",  .type = CONFIG_TYPE_UINT  , .boolValue   = &configStayInLevelAfterStar},
