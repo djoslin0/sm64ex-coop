470b9bcfb2bdfa58941f853345a5968962603a68
Isaac <62234577+Isaac0-dev@users.noreply.github.com>
2022-02-26 12:02:00 +1000

    Fixed issues on Linux

--- src/pc/network/socket/domain_res.c
+++ src/pc/network/socket/domain_res.c
@@ -8,38 +8,27 @@
 #include <ws2tcpip.h>
 #else
 #include <netdb.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h> /* for strncpy */
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/ioctl.h>
-#include <netinet/in.h>
-#include <net/if.h>
-#include <arpa/inet.h>
-
 #endif
 
 
 void domain_resolution(void) {
-    struct in_addr addr;
-    char *host_name;
-    struct hostent *remoteHost;
-    char* domainname = "";
-    int i = 0;
-    remoteHost = gethostbyname(host_name);
-    i = 0;
-    if (remoteHost->h_addrtype == AF_INET) {
+  struct in_addr addr;
+  char *host_name;
+  struct hostent *remoteHost;
+  char* domainname = "";
+  host_name = configJoinIp;
+  if (host_name == NULL) {
+	  return;
+  }
+  int i = 0;
+  remoteHost = gethostbyname(host_name);
+  i = 0;
+  if (remoteHost->h_addrtype == AF_INET) {
 
     while (remoteHost->h_addr_list[i] != 0) {
           addr.s_addr = *(u_long *) remoteHost->h_addr_list[i++];
           domainname = inet_ntoa(addr);
           snprintf(configJoinIp, MAX_CONFIG_STRING, "%s", domainname);
     }
-
   }
 }
