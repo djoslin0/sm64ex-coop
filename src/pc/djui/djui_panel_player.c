3f3cee5c80fde77a7607bd0e9ae19062575f5dff
DMMaster636 <86197998+DMMaster636@users.noreply.github.com>
2022-07-19 14:02:20 +0200

    Update djui_panel_player.c

--- src/pc/djui/djui_panel_player.c
+++ src/pc/djui/djui_panel_player.c
@@ -126,6 +126,8 @@ void djui_panel_player_create(struct DjuiBase* caller) {
             "Bubblegum",
             "Ice Mario",
             "Ice Luigi",
+            "SMB3 Mario",
+            "SMB3 Luigi",
         };
         struct DjuiSelectionbox* selectionbox2 = djui_selectionbox_create(&body->base, "Palette", paletteChoices, PALETTE_MAX, &configPlayerPalette);
         djui_base_set_size_type(&selectionbox2->base, DJUI_SVT_RELATIVE, DJUI_SVT_ABSOLUTE);
