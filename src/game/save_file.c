3d6fa714d8919713ff771ec2908659978dff8fa9
Dominicentek <dominicentekgaming@gmail.com>
2023-05-17 21:31:30 +0200

    Exposed save_file_is_cannon_unlocked to smlua

--- src/game/save_file.c
+++ src/game/save_file.c
@@ -657,8 +657,11 @@ s32 save_file_get_course_coin_score(s32 fileIndex, s32 courseIndex) {
 /**
  * Return TRUE if the cannon is unlocked in the current course.
  */
-s32 save_file_is_cannon_unlocked(void) {
-    return (gSaveBuffer.files[gCurrSaveFileNum - 1][gSaveFileUsingBackupSlot].courseStars[gCurrCourseNum] & 0x80) != 0;
+s32 save_file_is_cannon_unlocked(s32 fileIndex, s32 courseIndex) {
+    if (courseIndex == -1) {
+        return (gSaveBuffer.files[fileIndex][gSaveFileUsingBackupSlot].courseStars[gCurrCourseNum] & 0x80) != 0;
+    }
+    return (gSaveBuffer.files[fileIndex][gSaveFileUsingBackupSlot].courseStars[courseIndex] & 0x80) != 0;
 }
 
 /**
